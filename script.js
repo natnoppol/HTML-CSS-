82d2f8dd3b902e8e959f3acaa9bd440d5b87ff0e
natnoppol <126169607+natnoppol@users.noreply.github.com>
2023-10-23 13:14:23 +0200

    add
    add

---
+++ script.js
@@ -0,0 +1,8 @@
+let toggleDisplay= false;
+function onDisplayMenu() {
+    let menuMobile = document.getElementById("menu-mobile");
+    if (toggleDisplay = false) toggleDisplay = true;
+    else if (toggleDisplay = true) toggleDisplay = false;
+    if (toggleDisplay = true) menuMobile.style.display = "block";
+    else menuMobile.style.display = "none";
+}
